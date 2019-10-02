5896c8416bc80eb5ad34bc627f9ed63f4a1c7465
Ollenka <Ollenka@gmail.com>
2019-10-02 09:59:56 +0300

    Updates

---
+++ precache-manifest.2b2b5de37f2eb2305c21d6e860709c5b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6974f89822ad6aca15e57f92e95f8655",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "ed558143836aa83fb78a",
+    "url": "/ToDo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d7943acb08b5bf9bafbf",
+    "url": "/ToDo/static/css/main.b61e47cc.chunk.css"
+  },
+  {
+    "revision": "ed558143836aa83fb78a",
+    "url": "/ToDo/static/js/2.7ff64513.chunk.js"
+  },
+  {
+    "revision": "d7943acb08b5bf9bafbf",
+    "url": "/ToDo/static/js/main.91eacc5f.chunk.js"
+  },
+  {
+    "revision": "7f749822025ce21de9fd",
+    "url": "/ToDo/static/js/runtime-main.6abe815d.js"
+  },
+  {
+    "revision": "0994325dd5131eaca671e6a7426ac87f",
+    "url": "/ToDo/static/media/circle.0994325d.png"
+  },
+  {
+    "revision": "85e24632ff1665bf41fa8d052bb92fc6",
+    "url": "/ToDo/static/media/home.85e24632.png"
+  },
+  {
+    "revision": "79f99d7b11f61bf209cd6fa06b971385",
+    "url": "/ToDo/static/media/settings.79f99d7b.png"
+  }
+]);
